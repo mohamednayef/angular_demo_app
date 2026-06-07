9602da07d37345c57ead2756e70727cb43900914
mohamednayef <mohamednayef2002@gmail.com>
2026-06-07 10:01:49 +0300

    a commit'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TableGridComponent } from './components/table-grid/table-grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'table-grid',
+        component: TableGridComponent
+    },
+    {
+        path: '**',
+        component: HomeComponent
+    }
+];
